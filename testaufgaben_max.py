17d0bb60bfaeea5f2d2db88077857288778bbefa
Timm <timmwarstat@googlemail.com>
2021-02-23 10:08:33 +0100

    Aufgaben fuer Max

---
+++ testaufgaben_max.py
@@ -0,0 +1,91 @@
+
+
+# 1)***erstelle folgende Variablen:
+
+# TYPE        NAME            INHALT 
+
+# String      Name            Max
+# String      NachName        Warstat
+# int         Klasse          *deine Klasse*
+# float       Gewicht         dein Gewicht in kg mit Nachkommastellen
+# list        PyGruppe       Liste mit Strings der Namen aller Mitglieder in der Python-Whatsapp-Gruppe
+# int         x               fünf
+
+
+
+
+# 2)**gib folgenden Text aus. Benutze dabei die oben definierten Variablen:
+# "Hallo mein Name ist <<NAME>> <<NachName>>"
+
+
+# 3)***gib folgenden Text aus. Benutze dabei die oben definierten Variablen: (achte auf die Datentypen)
+#  "ich gehe in die <<Klasse>>. Klasse"
+
+
+# 4)***gib folgenden Text aus. Benutze dabei die oben definierten Variablen:
+# Benutze diesmal einen f-String:
+# "Hallo mein Name ist <<NAME>> <<NachName>> ich gehe in die <<Klasse>>. Klasse"
+
+
+# 5)***gib das 3. Element aus der Liste PyGruppe aus 
+
+
+# ----------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------
+
+
+# 6)***Erzeuge eine While Schleife die sich wiederholt, solange deine variable Klasse größer ist als x
+# Bei jedem durchlauf soll die Variable x um 1 erhöht werden. 
+
+
+
+
+# 7)***Nutze eine For-Schleife um durch jedes Element aus der Liste PyGruppe zu laufen
+# Gib bei jedem durchlauf das element aus. (1Name pro Durchlauf, sodass am Ende in der Konsole alle Namen der Gruppe in einer einzelnen Zeile stehen)
+# (Benutze NICHT list[index] in deinem print befehl)
+
+
+
+
+# 8)***Füge Tommy zur Liste PyGruppe hinzu
+
+
+# 9)***Gib die Anzahl der Elemente in PyGruppe aus
+
+
+# 10)**Gib das vorletzte Element der List PyGruppe aus. 
+
+
+
+
+# ----------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------
+
+# 11)***erstelle eine if-Abfrage ob x größer/gleich 15
+#     Wenn ja: setze den Wert 12 für x
+#     wenn nein: gib den Text "Variable x hat den Wert <<x>>"  (<<x>> = dein Wert der variable x )
+
+
+
+
+
+
+
+# 12)***Erzeuge erneut eine For Schleife für die Elemente in PyGruppe 
+#     innerhalb der Forschleife:
+#     erstelle eine IF abfrage 
+#     ob x>10
+#     Wenn ja: gib den Namen des Elements aus 
+#     Wenn nein: verringere den Wert der Variable x um 1 
+
+
+
+
+
+
+
+
+
+
